528787e029e8beee5eaa8cf37c89df53d29d4e03
Mirco Bisoffi <mircobisoffi@MacBook-Pro-di-Mirco.local>
2022-04-29 15:28:20 +0200

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+
 const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
 
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './HTMLpages/homepage.html'));
+    res.sendFile('homepage.html', {root: __dirname + "/public"});
 });
 
 app.get('/profile', (req, res) => {
-    res.sendFile(path.join(__dirname, './HTMLpages/profile.html')); 
-    
+    res.sendFile('profile.html', {root: __dirname + "/public"});
 });
 
 
 
 //GESISCE LE PAGINE CHE NON SONO VALIDE
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './HTMLpages/error.html'));
+    res.sendFile('error.html', {root: __dirname + "/public"});
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
