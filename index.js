8686b8469a3f9b2b4dc0ea4eadd4a3e72ee6257c
Mirco Bisoffi <mircobisoffi@MacBook-Pro-di-Mirco.local>
2022-04-29 16:16:20 +0200

    upd prove

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 
 const express = require('express')
 const path = require('path')
+const util = require('util')
 const app = express()
 const port = 3000
 
-
 app.use(express.static('public'))
+
+//GESTISCI TUTTE LE RICHIESTE
+app.all('/*', (req, res, next) =>{
+    console.log(req.url);
+    next();
+});
+
 
 app.get('/', (req, res) => {
     res.sendFile('homepage.html', {root: __dirname + "/public"});
+    
+    //res.json({ user: 'tobi' })
+
 });
 
 app.get('/profile', (req, res) => {
@@ -16,10 +26,26 @@ app.get('/profile', (req, res) => {
 });
 
 
+app.get('/search', function(req, res){
+    console.log(util.inspect(req.headers, {showHidden: false, depth: null}))
+    console.log(util.inspect(req.url, {showHidden: false, depth: null}))
+    console.log(util.inspect(req.query, {showHidden: false, depth: null}))
+    res.status(200).send('Check console!');
+});
+
+app.post('/subscribe', function(req, res){
+    console.log(util.inspect(req.headers, {showHidden: false, depth: null}))
+    console.log(util.inspect(req.params, {showHidden: false, depth: null}))
+    res.status(201).send('You are now subscribed!');
+    console.log(req.headers.val);
+});
 
 //GESISCE LE PAGINE CHE NON SONO VALIDE
 app.get('/*', (req, res) => {
     res.sendFile('error.html', {root: __dirname + "/public"});
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+});
+
