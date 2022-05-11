e22b4588321ab48564f1df89013db501cca8e199
mircoloo <bismirco@gmail.com>
2022-05-11 16:19:06 +0200

    upd

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 require('./scripts/dbConnection')
+const expressLayouts = require('express-ejs-layouts') 
 
 
 const port = process.env.PORT || 3000
 const app = express()
 
 //MIDDLEWARES
-app.use(express.static(path.join(__dirname, "/public")))
+
 app.use("/styles",  express.static(__dirname + '/public/stylesheets'));
 app.use("/scripts", express.static(__dirname + '/public/javascripts'));
 app.use("/images",  express.static(__dirname + '/public/images'));
@@ -18,25 +19,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-//ROUTES
+//SETS
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static(path.join(__dirname, "/public")))
+
+
+//ROUTER
+const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users');
+
+//ROUTES
+app.use('/', indexRouter)
+
 app.use('/users', usersRouter);
-
-
-app.use((req, res, next) => {
-    console.log(req.url)
-    next()
-})
-
-//
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, "public/login.html"))
-});
- 
-
-app.use((req,res) =>{
-    res.status(404).json({error: '404'});
-});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
